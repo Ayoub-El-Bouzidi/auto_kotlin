3460c3507f7cad5b48c5f7e5c7719730a15c64c2
Ayoub-El-Bouzidi <153141906+Ayoub-El-Bouzidi@users.noreply.github.com>
2024-12-02 00:58:24 +0100

    Delete app/src/main/java/com/example/miniproject directory

--- app/src/main/java/com/example/miniproject/data/affirmations.kt
+++
@@ -1,9 +0,0 @@
-package com.example.miniproject.data
-
-val affirmations = listOf(
-    "Crois en toi-même et en toutes tes capacités.",
-    "Chaque jour est une nouvelle opportunité.",
-    "Tu es plus fort que tu ne le penses.",
-    "N'abandonne jamais, les miracles prennent du temps.",
-    "Le succès commence par la volonté de l'atteindre."
-)
