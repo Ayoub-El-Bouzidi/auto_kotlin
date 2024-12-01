ac92891f23933c97af4896835b18c6123c1b0122
Ayoub <elbouzidiayoub.solicode@gmail.com>
2024-12-02 00:55:25 +0100

    miniProject

---
+++ app/src/main/java/com/example/miniproject/data/affirmations.kt
@@ -0,0 +1,9 @@
+package com.example.miniproject.data
+
+val affirmations = listOf(
+    "Crois en toi-même et en toutes tes capacités.",
+    "Chaque jour est une nouvelle opportunité.",
+    "Tu es plus fort que tu ne le penses.",
+    "N'abandonne jamais, les miracles prennent du temps.",
+    "Le succès commence par la volonté de l'atteindre."
+)
